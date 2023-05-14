5429924d378410445a9153de19b2fcb40387226c
efriedin <erin.friedman@ymail.com>
2023-05-14 12:30:04 -0500

    added API url

--- week-15-project/src/App.jsx
+++ week-15-project/src/App.jsx
@@ -1,37 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from './components/Header'
 import './App.css'
+import Table from './components/Table'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const MOCK_API_URL = 'https://643efbc0b9e6d064beec702e.mockapi.io/'
+  const [users, setUsers] = useState([{
+    name: "",
+    jobTitle: "",
+    companyName: "",
+  }])
+
+  useEffect(() => {
+    fetch(MOCK_API_URL)
+    .then((data) => data.json())
+    .then((data) => console.log(data))
+    .then((data) => setUsers(data))
+  }, [])
+
+  const getUsers = () => {
+    console.log(`doing get users function`)
+
+    fetch(MOCK_API_URL)
+    .then((data) => data.json())
+    .then((data) => setUsers(data))
+  }
+
+  // const postUser = () => {
+
+  // }
+
+  // const deleteUser = () => {
+
+  // }
+
+  // const updateUser = () => {
+
+  // }
 
   return (
-    <>
-      <div>
-        <Header />
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <Table />
+    </div>
   )
 }
 
